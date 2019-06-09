d57b0c6d91c2a3507f5c1aee3a9297bb398119b2
A <aryjna1@gmail.com>
2019-06-09 13:37:20 +0300

    removed body-parser

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(port, function() {
   console.log(`Server listening on port ${port}`);
