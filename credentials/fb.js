51239bcdca55ee40e33777ea25eef587d0112f9e
coderharshit <harshitbhalla.hb@gmail.com>
2019-06-10 12:04:08 +0530

    update callback url

--- credentials/fb.js
+++ credentials/fb.js
@@ -1,7 +1,7 @@
 const fb = {
   "clientID": "627292194437325",
   "clientSecret": "7017d8be079b2c6900ffc40660741c26",
-  "callbackURL": "http://www.localhost:3001/auth/facebook/callback"
+  "callbackURL": "https://secret-chamber-38375.herokuapp.com/auth/facebook/callback"
 }
 
 module.exports = fb;
